051193f6c7cdee6b352d8c1f20f1bf2330475940
madansah960860-prog <madansah960860@gmail.com>
2026-08-19 09:12:27 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- Privacy-friendly analytics by Plausible -->
+<script async src="https://plausible.io/js/pa-RfDKT_236nYodvoYjTz2v.js"></script>
+<script>
+  window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+  plausible.init()
+</script>
+
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Maison Noir | Tasting Menu Restaurant in New York, Washington DC and San Francisco</title>
